14523b0d5a0ca386c458cbdd6534376ed1c89ffd
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-09-14 15:26:12 +0000

    commit

--- test/test.js
+++ test/test.js
@@ -12,14 +12,13 @@ describe('Example HW', function() {
 });
 
 describe('DB Tests', function(){
+    
    describe('Check Password Fails', function() {
-      it('should return false when the password is not matching', function() {
-            var testUser = new User({
-                user_name: 'jmar7774',
-                user_password: 'Password123',
-                user_location: 'Seattle, WA'
-            });
+      it('should return false when the password is not matching', function(done) {
+            
             mongoose.connect('mongodb://dbuser:password@ds019796.mlab.com:19796/th-book-exchange'); 
+
+
             User.findOne({ user_name: 'jmar7774' }, function(err, user) {
                 if (err) throw err;
                 user.comparePassword('123Password', function(err, isMatch) {
@@ -27,28 +26,59 @@ describe('DB Tests', function(){
                     console.log('123Password:', isMatch); // -&gt; 123Password: false
                     assert.equal(false, isMatch);
                 });
-            });  
+            });
+           
+            done();
       });
   });
   
   
   
    describe('Check Password Succeds', function() {
-      it('should return true when the password is matching', function() {
-            var testUser = new User({
-                user_name: 'jmar7774',
-                user_password: 'Password123',
-                user_location: 'Seattle, WA'
-            });
+      it('should return true when the password is matching', function(done) {
+          
             User.findOne({ user_name: 'jmar7774' }, function(err, user) {
                 if (err) throw err;
                 user.comparePassword('Password123', function(err, isMatch) {
                     if (err) throw err;
                     console.log('Password123:', isMatch); // -&gt; Password123: true
-                    assert.equal(false, isMatch);
+                    assert.equal(true, isMatch);
                 });
             });  
+            
+            done();
+
        });
+       
+       
+  });
+
+    
+     describe('Save new Users Succeds', function() {
+      it('should return true when the user was saved in the db', function(done) {
+            var testUserCreation = new User({
+                user_name: 'jmar7774@test.com',
+                user_password: 'Password1234',
+                user_location: 'Seattle, WA'
+            });
+            
+            testUserCreation.save(function(err) {
+                if (err) throw err;
+            }); 
+            
+            User.findOne({ user_name: 'jmar7774@test.com' }, function(err, user) {
+                    if (err) throw err;
+                    user.comparePassword('Password1234', function(err, isMatch) {
+                        if (err) throw err;
+                        console.log('Password1234:', isMatch); // -&gt; Password123: true
+                        assert.equal(true, isMatch);
+                    });
+             });
+             mongoose.connection.close();
+             done();
+             
+       });
+       
   });
 
   
